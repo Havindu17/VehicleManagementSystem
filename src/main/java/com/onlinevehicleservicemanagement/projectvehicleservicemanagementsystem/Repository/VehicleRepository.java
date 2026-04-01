7c259ff2cce87df16a6bdceeac3a02de542b3bfd
IT24103016 <IT24103016@MY.SLIIT.LK>
2026-04-01 14:40:52 +0530

    Vehicle Management

---
+++ src/main/java/com/onlinevehicleservicemanagement/projectvehicleservicemanagementsystem/Repository/VehicleRepository.java
@@ -0,0 +1,4 @@
+package com.onlinevehicleservicemanagement.projectvehicleservicemanagementsystem.Repository;
+import com.onlinevehicleservicemanagement.projectvehicleservicemanagementsystem.Model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {}
